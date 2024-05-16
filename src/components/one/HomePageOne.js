3e6f4c81677816c5b9c80048a1389a3eced8fbd8
ChernyshovNikita <nikita@fundingpips.com>
2024-05-16 15:17:22 +0400

    feat: serve npm for static deployment

--- src/components/one/HomePageOne.js
+++ src/components/one/HomePageOne.js
@@ -1,9 +1,13 @@
 import { Block } from "../common/Block";
+import { Link } from "react-router-dom";
 
 export const HomePageOne = () => {
   return (
     <div>
       <h2>Home Page One</h2>
+      <nav>
+        <Link to="about">about</Link>
+      </nav>
 
       <Block />
     </div>
--- src/components/two/HomePageTwo.js
+++ src/components/two/HomePageTwo.js
@@ -1,9 +1,13 @@
 import { Block } from "../common/Block";
+import { Link } from "react-router-dom";
 
 export const HomePageTwo = () => {
   return (
     <div>
       <h2>Home Page Two</h2>
+      <nav>
+        <Link to="about">about</Link>
+      </nav>
 
       <Block />
     </div>
